6a7ff48f1d275fcbe6a29f8dc6bb2d2c1b95b80b
Scott <scoberts@yahoo.com>
2016-02-07 11:21:09 -0800

    Plot2

---
+++ plot2.R
@@ -0,0 +1,16 @@
+#Plot 2
+household_power_consumption <- read.csv("~/Documents/workspace/PlottingData/household_power_consumption.txt", sep=";")
+power <- household_power_consumption
+power$DateTime <- paste(power$Date, power$Time)
+plotdata <- subset(power, DateTime < as.POSIXct('2007-02-03'))
+plotdata <- subset(plotdata, DateTime > as.POSIXct('2007-02-01'))
+plotdata$Global_active_power <- as.numeric(plotdata$Global_active_power)
+
+#Build the Line Plot
+plot(plotdata$DateTime, plotdata$Global_active_power/1000, type="n", xlab="", ylab="")
+with(plotdata, lines(DateTime,Global_active_power/1000))
+title(ylab="Global Active Power (kilowatts)")
+
+
+dev.copy(png, file="plot2.png", width=480, height=480, units="px")
+dev.off()
